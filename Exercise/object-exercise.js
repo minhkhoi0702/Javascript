2df65ba4715bc6710b5625ecf41d32371b1b957b
johnny <minhkhoi0702@gmail.com>
2023-05-29 11:53:00 +0700

    update lesson

--- Exercise/object-exercise.js
+++ Exercise/object-exercise.js
@@ -21,36 +21,52 @@ function objectToArray(object) {
   for (let key in object) {
     // hasOwnProperty(key) -> nếu object chứa key trả về true ngược lại trả về false
     // if (object.hasOwnProperty(key)){}
-    result.push([key,object[key]])
+    result.push([key, object[key]]);
   }
-  return result
+  return result;
 }
 // console.log(objectToArray({ a: 1, b: 2 }));
 
-// without 
+// [["a",1], ["b",2]] -> {a:1,b:2}
+
+function arrToObj(arr) {
+  const newObj = arr.reduce((obj, curr)=>{
+    obj[curr[0]] = curr[1]
+    return obj
+  }, {});
+  return newObj
+}
+// console.log(
+//   arrToObj([
+//     ["a", 1],
+//     ["b", 2],
+//   ])
+// );
+
+// without
 // ({a: 1, b: 2}, b) =>{a: 1}
-function without(object,...key) {
-    const newObject = {...object}
-    console.log(key)
-    key.forEach((item)=>{
-        delete newObject[item]
-    });
-    
-    return newObject
+function without(object, ...key) {
+  const newObject = { ...object };
+  console.log(key);
+  key.forEach((item) => {
+    delete newObject[item];
+  });
+
+  return newObject;
 }
 // console.log(without({a: 1, b: 2}, "a","b"))
 
 // 4. {a: 1, b: 2}, {a: 1, b: 2} => true
 // {a: 1, b: 2}, {a: 1, b: 2, c:3} => false
-function isEqualObject(obj1,obj2){
+function isEqualObject(obj1, obj2) {
   // check key lenght of two objects
   const objKey1 = Object.keys(obj1);
   const objKey2 = Object.keys(obj2);
-  if(objKey1.length !== objKey2.length) return false;
+  if (objKey1.length !== objKey2.length) return false;
   // check values of two object
-  const result = objKey1.every((key) => { return obj1[key] === obj2[key]})
-  return result
-  
-  
+  const result = objKey1.every((key) => {
+    return obj1[key] === obj2[key];
+  });
+  return result;
 }
-console.log(isEqualObject({a: 1, b: 2}, {a: 1, b: 2}))
+// console.log(isEqualObject({a: 1, b: 2}, {a: 1, b: 2}))
